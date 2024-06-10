3b06aa01655c93e11eff87725e576321404c04c9
Flavio-Converso <fla.converso@gmail.com>
2024-06-10 15:08:29 +0200

    .

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,20 +1,68 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorMailPasswordService } from '../validator-mail-password.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss',
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  registerForm!: FormGroup;
+  formSubmitted = false;
+
   constructor(
     private authSvc: AuthService,
     private router: Router,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private validatorSvc: ValidatorMailPasswordService
   ) {}
 
-  ngOnInit() {}
-  //register() {}
+  ngOnInit() {
+    this.registerForm = this.fb.group({
+      nome: [null, [Validators.required]],
+      cognome: [null, [Validators.required]],
+      authData: this.fb.group(
+        {
+          email: [
+            null,
+            [Validators.required, this.validatorSvc.isValidEmail()],
+          ],
+          confermaEmail: [null, [Validators.required]],
+          password: [
+            null,
+            [Validators.required, this.validatorSvc.isValidPassword()],
+          ],
+          confermaPassword: [null, [Validators.required]],
+        },
+        {
+          validators: [
+            this.validatorSvc.emailMatch,
+            this.validatorSvc.passwordMatch,
+          ],
+        }
+      ),
+      genere: ['selectPlaceholder', [Validators.required]],
+      immagine: [null],
+      username: [null, [Validators.required]],
+    });
+  }
+
+  register() {
+    this.formSubmitted = true;
+    if (this.registerForm.valid) {
+      this.authSvc.register(this.registerForm.value).subscribe(() => {
+        this.router.navigate(['auth/login']);
+      });
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+
+  isTouchedInvalid(fieldName: string) {
+    const field = this.registerForm.get(fieldName);
+    return field?.invalid && field?.touched;
+  }
 }
