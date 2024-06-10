4e7137307d083c5c8f5db8bdd1ca58e4be70edec
Flavio-Converso <fla.converso@gmail.com>
2024-06-10 15:31:30 +0200

    .

--- src/app/main-components/navbar/navbar.component.ts
+++ src/app/main-components/navbar/navbar.component.ts
@@ -3,16 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']  // Corretto qui
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
   isCollapsed: boolean = true;
-
-  iscoll2: boolean = false;
-
-  iscoll: boolean = false;
-  logscoll: boolean = false;
-
   openDropdown: string | null = null;
 
   toggleDropdown(dropdown: string): void {
