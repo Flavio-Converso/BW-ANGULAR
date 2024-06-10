b8525a7617a69882e8d84694ce31b7e25c951f6d
Manuel Pinheiro <manuel.pinheiro@libero.it>
2024-06-10 15:00:38 +0200

    nav base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCollapseModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './main-components/navbar/navbar.component';
 import { FooterComponent } from './main-components/footer/footer.component';
 import { Page404Component } from './dashboard/page404/page404.component';
@@ -15,7 +15,7 @@ import { Page404Component } from './dashboard/page404/page404.component';
     FooterComponent,
     Page404Component,
   ],
-  imports: [BrowserModule, AppRoutingModule, NgbModule],
+  imports: [BrowserModule, AppRoutingModule, NgbModule,NgbCollapseModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/main-components/navbar/navbar.component.ts
+++ src/app/main-components/navbar/navbar.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrls: ['./navbar.component.scss']  // Corretto qui
 })
 export class NavbarComponent {
+  isCollapsed: boolean = true;
 
+  iscoll2: boolean = false;
+
+  iscoll: boolean = false;
+  logscoll: boolean = false;
+
+
+  // logout() {
+  //   this.authSvc.logout();
+  // }
 }
