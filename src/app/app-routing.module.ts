13e8e28fbb7dc53716b678429c5a3eb5fb624089
Flavio-Converso <fla.converso@gmail.com>
2024-06-10 11:26:29 +0200

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }, { path: 'schedaPg', loadChildren: () => import('./scheda-pg/scheda-pg.module').then(m => m.SchedaPgModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
