76d8f6c2ff51c3a2aef49b9e99f64a81a30d51e6
Flavio-Converso <fla.converso@gmail.com>
2024-06-11 09:55:09 +0200

    interfacce per creazione pg

---
+++ src/app/interfaces/classe.ts
@@ -0,0 +1,6 @@
+export interface iClassi {
+  classId: number;
+  className: string;
+  bio: string;
+  skills: number[];
+}
---
+++ src/app/interfaces/skills.ts
@@ -0,0 +1,6 @@
+export interface iSkills {
+  skillId: number;
+  skillName: string;
+  skillDescription: string;
+  exp: number;
+}
