2db27fb770d8567a0dfc8d42ac4e2822d22aa4ac
Flavio-Converso <fla.converso@gmail.com>
2024-06-11 09:22:47 +0200

    .

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { iUsers } from '../interfaces/iusers';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersService {
+  apiUrl: string = 'http://localhost:3000/users';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAllUsers(): Observable<iUsers[]> {
+    return this.http.get<iUsers[]>(this.apiUrl);
+  }
 }
