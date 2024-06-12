fc6a85df889a5b3ef6f852ba624f7e48b4226573
Ciraldos <andreaciraldo2@gmail.com>
2024-06-12 17:14:08 +0200

    .

--- src/app/dashboard/scheda-pg/scheda-pg.component.ts
+++ src/app/dashboard/scheda-pg/scheda-pg.component.ts
@@ -1,7 +1,10 @@
+import { ClassesService } from './../../services/classes.service';
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CharactersService } from '../../services/characters.service';
 import { iCharacter } from '../../interfaces/icharacter';
+import { SkillsService } from '../../services/skills.service';
+import { iSkills } from '../../interfaces/skills';
 
 @Component({
   selector: 'app-scheda-pg',
@@ -11,11 +14,16 @@ import { iCharacter } from '../../interfaces/icharacter';
 export class SchedaPgComponent {
   characterId!: number;
   character!: iCharacter;
+  allSkills: iSkills[] = [];
+  characterSkills: iSkills[] = [];
+
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private charactersSvc: CharactersService
+    private charactersSvc: CharactersService,
+    private skillSrc:SkillsService,
+    private classSrc:ClassesService
   ) { }
 
   ngOnInit(): void {
@@ -29,8 +37,24 @@ export class SchedaPgComponent {
     this.charactersSvc.getCharacterById(this.characterId).subscribe((character: iCharacter) => {
       this.character = character;
       console.log('Character details:', this.character);
+      this.loadCharacterSkills();
     });
   }
+
+  loadCharacterSkills(): void {
+    this.skillSrc.getSkills().subscribe((skills: iSkills[]) => {
+      this.allSkills = skills;
+      if (this.character && this.character.selectedSkills) {
+        this.characterSkills = this.allSkills.filter(skill =>
+          this.character.selectedSkills!.includes(skill.skillId)
+        );
+        console.log('Character skills:', this.characterSkills);
+      }
+    });
+  }
+
+
+
 
   deleteCharacter(): void {
     if (confirm('Sei sicuro di voler eliminare questo personaggio?')) {
