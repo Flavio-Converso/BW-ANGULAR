a40e66f2223b1820a98fe4efede7df35fba16b27
AleNerima <ale95polioni@gmail.com>
2024-06-12 16:39:36 +0200

    pagina funzionante degli active users

--- src/app/dashboard/active-users/active-users.component.ts
+++ src/app/dashboard/active-users/active-users.component.ts
@@ -11,62 +11,76 @@ import { iClassi } from '../../interfaces/classe';
 import { iSkills } from '../../interfaces/skills';
 import { SkillsService } from '../../services/skills.service';
 
-
-
-
 @Component({
   selector: 'app-active-users',
   templateUrl: './active-users.component.html',
-  styleUrl: './active-users.component.scss'
-
+  styleUrls: ['./active-users.component.scss']
 })
 export class ActiveUsersComponent {
   user!: iUsers | null;
-  iSkills: iSkills[]=[];
+  iSkills: iSkills[] = [];
   characters: iCharacter[] = [];
-  class: iClassi[]=[];
+  class: iClassi[] = [];
   combina: iCombinazione[] = [];
   users: iUsers[] = [];
   isCollapsed: boolean = true;
   openDropdown: string | null = null;
 
-  constructor(private authSvc: AuthService, private userSvc: UsersService,  private characterSvc: CharactersService, private classSvc: ClassesService,
-    private combinaSvc: CombinaService,private skillSvc: SkillsService ) {}
+  constructor(
+    private authSvc: AuthService,
+    private userSvc: UsersService,
+    private characterSvc: CharactersService,
+    private classSvc: ClassesService,
+    private combinaSvc: CombinaService,
+    private skillSvc: SkillsService
+  ) {}
 
   ngOnInit() {
     this.authSvc.user$.subscribe((user: iUsers | null) => {
       this.user = user;
       if (this.user) {
-        this.characterSvc.getCharactersByUserId(this.user.id).subscribe((chars: iCharacter[]) => {
-          this.characters = chars;
-          this.addToCombina();
-        });
-        this.classSvc.getClassByUserId(this.user.id).subscribe((caClass: iClassi[]) => {
-          this.class = caClass;
-          this.addToCombina();
-        });
-
-         this.skillSvc.getSkillByUserId(this.user.id).subscribe((kSkills:iSkills[])=>{
-         this.iSkills =kSkills;
-         this.addToCombina();
-        });
-      }});
-
-
-    this.userSvc.getAllUsers().subscribe((users) => {
-      this.users = users
+        this.fetchAllData();
+      }
     });
 
-
-
-
+    console.log("Fetching all users...");
+    this.userSvc.getAllUsers().subscribe((users) => {
+      console.log("All users received:", users);
+      this.users = users;
+    });
   }
 
-  addToCombina() {
-    if (this.characters.length > 0 && this.class.length > 0 && this.iSkills.length > 0) {
-      // Chiamata al servizio per combinare i dati
-      this.combina = this.combinaSvc.combineData(this.characters, this.class, this.iSkills);
+  fetchAllData() {
+    console.log("Fetching all data for user:", this.user);
+    this.characterSvc.getCharacters().subscribe((chars: iCharacter[]) => {
+      console.log("All characters received:", chars);
+      this.characters = chars;
+      this.fetchClassAndSkills();
+    });
+  }
+
+  fetchClassAndSkills() {
+    console.log("Fetching class and skills for user:", this.user);
+    if (this.user !== null && this.user !== undefined) {
+      this.classSvc.getClassByUserId(this.user.id).subscribe((caClass: iClassi[]) => {
+        console.log("Classes received:", caClass);
+        this.class = caClass;
+        this.skillSvc.getSkillByUserId(this.user!.id).subscribe((kSkills: iSkills[]) => {
+          console.log("Skills received:", kSkills);
+          this.iSkills = kSkills;
+          this.addToCombina();
+        });
+      });
+    } else {
+      console.error("User is null or undefined");
     }
   }
 
+  addToCombina() {
+    console.log("Adding to combina...");
+    if (this.characters.length > 0 && this.class.length > 0 && this.iSkills.length > 0) {
+      console.log("Combining data...");
+      this.combina = this.combinaSvc.combineData(this.characters, this.class, this.iSkills);
+    }
+  }
 }
