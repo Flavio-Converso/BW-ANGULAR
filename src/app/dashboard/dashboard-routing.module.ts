7bbffb0ddbb8805f92f3b1706c498dc69c402adc
Manuel Pinheiro <manuel.pinheiro@libero.it>
2024-06-13 14:35:14 +0200

    mod rooting

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -5,6 +5,7 @@ import { DashboardComponent } from './dashboard.component';
 import { ActiveUsersComponent } from './active-users/active-users.component';
 import { CreazionePgComponent } from './creazione-pg/creazione-pg.component';
 import { SchedaPgComponent } from './scheda-pg/scheda-pg.component';
+import { MarketplaceComponent } from './marketplace/marketplace.component';
 
 
 const routes: Routes = [
@@ -12,7 +13,8 @@ const routes: Routes = [
   { path: 'active_users', component:ActiveUsersComponent},
   {path:'profile', component: ProfileComponent},
 { path: 'newpg', component: CreazionePgComponent},
-{ path: 'schedapg/:id', component: SchedaPgComponent}
+{ path: 'schedapg/:id', component: SchedaPgComponent},
+{path: 'eventi',component: MarketplaceComponent}
 ];
 
 @NgModule({
