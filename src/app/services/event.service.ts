0b7ad70b5620b5e5f71fee5dad7ef341139f62c9
AleNerima <ale95polioni@gmail.com>
2024-06-13 15:44:18 +0200

    fine creazione service eventi

--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -1,64 +1,53 @@
-
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
 import { Observable } from 'rxjs';
+import { iEventi } from '../interfaces/ieventi';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
-    private apiUrl = 'http://localhost:3000/characters';
+  private apiUrl = 'http://localhost:3000/eventi';
 
-    constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-    getCharacters(): Observable<iCharacter[]> {
-      return this.http.get<iCharacter[]>(this.apiUrl);
-    }
-
-    getCharacterById(id: number): Observable<iCharacter> {
-      return this.http.get<iCharacter>(`${this.apiUrl}/${id}`);
-    }
-
-
-    getCharactersByUserId(userId: number): Observable<iCharacter[]> {
-      const url = `${this.apiUrl}?userId=${userId}`;
-      return this.http.get<iCharacter[]>(url);
-    }
-
-
-    addCharacter(newCharacter: iCharacter): Observable<iCharacter> {
-      return this.http.post<iCharacter>(this.apiUrl, newCharacter);
-    }
-
-    updateCharacter(
-      id: number,
-      updatedCharacter: iCharacter
-    ): Observable<iCharacter> {
-      return this.http.put<iCharacter>(
-        `${this.apiUrl}/${id}`,
-        updatedCharacter
-      );
-    }
-
-    deleteCharacter(id: number): Observable<void> {
-      return this.http.delete<void>(`${this.apiUrl}/${id}`);
-    }
+  getEventi(): Observable<iEventi[]> {
+    return this.http.get<iEventi[]>(this.apiUrl);
   }
 
+  getEventiById(id: number): Observable<iEventi> {
+    return this.http.get<iEventi>(`${this.apiUrl}/${id}`);
+  }
 
+  getEventiByUserId(userId: number): Observable<iEventi[]> {
+    const url = `${this.apiUrl}?userId=${userId}`;
+    return this.http.get<iEventi[]>(url);
+  }
 
+  addEventi(newEventi: iEventi): Observable<iEventi> {
+    return this.http.post<iEventi>(this.apiUrl, newEventi);
+  }
 
+  updateEventi(id: number, updatedEventi: iEventi): Observable<iEventi> {
+    return this.http.put<iEventi>(`${this.apiUrl}/${id}`, updatedEventi);
+  }
 
-
-
-
-
-
-
-
-
-
-
-
+  deleteEventi(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
